f1291b24234cb8811ec095023c537d364d5e6b85
Jonathan Martinez Hernandez <jonathan.martinez@fygdevelopers.com>
2020-09-03 13:02:25 -0500

    skeleton

---
+++ src/main/java/mx/com/multiva/sipare/GetBalanceApplication.java
@@ -0,0 +1,13 @@
+package mx.com.multiva.sipare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetBalanceApplication.class, args);
+	}
+
+}
