da135585ace9b8984c28c07b9715e6892f4f4d8f
Jonathan Martinez Hernandez <jonathan.martinez@fygdevelopers.com>
2020-09-03 14:41:18 -0500

    Model - Message as response

---
+++ src/main/java/mx/com/multiva/sipare/model/response/Message.java
@@ -0,0 +1,44 @@
+package mx.com.multiva.sipare.model.response;
+
+import java.sql.Timestamp;
+
+public class Message {
+
+    private int status;
+    private String error;
+    private String message;
+    private Timestamp timestamp;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+}
