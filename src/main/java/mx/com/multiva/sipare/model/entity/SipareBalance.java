fe6274c7151b7806a24039d8a8ee89c3575349a9
Jonathan Martinez Hernandez <jonathan.martinez@fygdevelopers.com>
2020-09-03 14:14:11 -0500

    Model - Entity

---
+++ src/main/java/mx/com/multiva/sipare/model/entity/SipareBalance.java
@@ -0,0 +1,86 @@
+package mx.com.multiva.sipare.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "sipare_balances")
+public class SipareBalance {
+
+    @Id
+    private int id;
+    private String dispatchDate;
+    private String type;
+    private String paymentDate;
+    private BigDecimal totalRcv;
+    private BigDecimal totalVicAcvImss;
+    private BigDecimal total;
+    private int status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDispatchDate() {
+        return dispatchDate;
+    }
+
+    public void setDispatchDate(String dispatchDate) {
+        this.dispatchDate = dispatchDate;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(String paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public BigDecimal getTotalRcv() {
+        return totalRcv;
+    }
+
+    public void setTotalRcv(BigDecimal totalRcv) {
+        this.totalRcv = totalRcv;
+    }
+
+    public BigDecimal getTotalVicAcvImss() {
+        return totalVicAcvImss;
+    }
+
+    public void setTotalVicAcvImss(BigDecimal totalVicAcvImss) {
+        this.totalVicAcvImss = totalVicAcvImss;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+}
