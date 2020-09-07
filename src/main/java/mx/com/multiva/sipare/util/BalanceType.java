87d61d120fd08683a4725b648fe479a6aa1fdfff
Jonathan Martinez Hernandez <jonathan.martinez@fygdevelopers.com>
2020-09-07 15:47:47 -0500

    Util - Balances type

---
+++ src/main/java/mx/com/multiva/sipare/util/BalanceType.java
@@ -0,0 +1,12 @@
+package mx.com.multiva.sipare.util;
+
+public final class BalanceType {
+
+    private BalanceType() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String PROCESAR = "PROCESAR";
+    public static final String CONSAR = "CONSAR";
+
+}
