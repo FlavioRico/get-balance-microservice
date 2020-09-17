9b3d1213a938bb782501c41234288e11b4810d67
Jonathan Martinez Hernandez <jonathan.martinez@fygdevelopers.com>
2020-09-17 05:43:38 -0500

    URL refactoring

--- src/main/java/mx/com/multiva/sipare/util/client/AccountBalanceT24Client.java
+++ src/main/java/mx/com/multiva/sipare/util/client/AccountBalanceT24Client.java
@@ -4,7 +4,7 @@ import mx.com.multiva.sipare.model.util.AccountBalanceT24;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(url = "http://10.160.14.213:8083", name = "accountBalanceT24")
+@FeignClient(url = "${yaxche.ribbon.listOfServers}", name = "accountBalanceT24")
 public interface AccountBalanceT24Client {
 
     @PostMapping("/sipareMSProcessFileApp/multiva/sipare/getAccountBalanceT24")
