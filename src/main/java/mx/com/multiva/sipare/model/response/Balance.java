b9c178b6fe00e1a4ec4f36690c7de63192ade6d8
Jonathan Martinez Hernandez <jonathan.martinez@fygdevelopers.com>
2020-09-17 03:08:46 -0500

    Balance Model refactoring

--- src/main/java/mx/com/multiva/sipare/model/response/Balance.java
+++ src/main/java/mx/com/multiva/sipare/model/response/Balance.java
@@ -2,57 +2,19 @@ package mx.com.multiva.sipare.model.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import mx.com.multiva.sipare.model.entity.SipareBalance;
 import org.springframework.http.HttpStatus;
 
-public class Balance {
+public class Balance extends SipareBalance {
 
-    private int id;
-    @JsonProperty("dispatch_date")
-    private String dispatchDate;
-    @JsonProperty("payment_date")
-    private String paymentDate;
-    private String type;
     @JsonProperty("file_amounts")
     private Summary fileAmounts;
     @JsonProperty("t24_amounts")
     private Summary t24Amounts;
     private Comparison comparisons;
     private boolean balanced;
-    private int status;
     @JsonIgnore
     private HttpStatus httpStatus;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getDispatchDate() {
-        return dispatchDate;
-    }
-
-    public void setDispatchDate(String dispatchDate) {
-        this.dispatchDate = dispatchDate;
-    }
-
-    public String getPaymentDate() {
-        return paymentDate;
-    }
-
-    public void setPaymentDate(String paymentDate) {
-        this.paymentDate = paymentDate;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
 
     public Summary getFileAmounts() {
         return fileAmounts;
@@ -84,14 +46,6 @@ public class Balance {
 
     public void setBalanced(boolean balanced) {
         this.balanced = balanced;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
     }
 
     public HttpStatus getHttpStatus() {
